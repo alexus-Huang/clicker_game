4840e075613196c6018bff71d9a240b1a1c4beef
Alexus Huang <alexushu2325@gmail.com>
2026-05-03 16:00:50 -0700

    feat: implement rebirth reset logic and UI warnings

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ root.minsize(1000,500)
 #Variables
 rate = 1.00
 clicks = 0
-
+rebirth_level = 0
 #Rebirth Data
 rebirth_costs = {
-    1: 10,
-    2: 50,
-    3: 100,
-    4: 500,
-    5: 1000
+    1: 5,
+    2: 10,
+    3: 15,
+    4: 20,
+    5: 1000,
+    6: 1500,
+    7: 2000,
+    8: 2500,
+    9: 3000,
+    10: 3500
 }
 
 
@@ -31,10 +36,26 @@ def rebirth_window():
 
         rebirth_label = tk.Label(popup,text = rebirth_text, font = ("Arial",12))
         rebirth_label.pack(pady=5)
+def show_warning(msg):
+    warningLabel.config(text=msg)
+    root.after(2000,lambda:warningLabel.config(text=""))
 
 def rebirth():
+    #check if user has correct amount of clicks to rebirth
+    global rebirth_level
     global rate
-    rate += 0.2
+    global clicks
+
+    cost = rebirth_costs.get(rebirth_level + 1, float('inf'))
+    if clicks >= cost:
+        rebirth_level+=1
+        clicks = 0
+        rate += 0.2
+        clicksLabel.config(text=f"Clicks: {clicks}")
+        rebirth_level_label.config(text=f"Rebirth level: {rebirth_level}")
+    else:
+        show_warning("⚠️ Not enough clicks to rebirth!")
+
     print(rate)
 
 def click():
@@ -50,6 +71,11 @@ titleLabel.pack()
 clicksLabel = tk.Label(root,text=f"Clicks {clicks}")
 clicksLabel.pack()
 
+rebirth_level_label = tk.Label(root,text=f"Rebirth Level:{rebirth_level}")
+rebirth_level_label.pack(pady=10)
+
+warningLabel = tk.Label(root,text="",fg="red",font=("Arial",12,"bold"))
+warningLabel.pack(pady=5)
 #buttons
 clickBtn = tk.Button(root,text = "Click!", command=click)
 clickBtn.pack()
